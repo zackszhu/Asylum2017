26a81d05f142abc40685e112a9e6ab68e0dd59a9
Tianyi Zhao <tz1@andrew.cmu.edu>
2017-11-03 14:01:46 -0400

    finish movement?

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private static CameraController Instance;
+
+    [SerializeField] float DirectionBias = 3f;
+    [SerializeField] float SpeedCoef = 4f;
+    private Vector3 PlayerRelCenterPos;
+
+    private PlayerController player;
+
+    private void Awake () {
+        if (!Instance) Instance = this;
+        else Debug.LogWarning("Multiple CameraController detected");
+    }
+
+    // Use this for initialization
+    void Start () {
+        player = PlayerController.Instance;
+        PlayerRelCenterPos = transform.position - player.transform.position;
+	}
+
+    private void MoveToUpdate (Vector3 pos) {
+        Vector3 rel = pos - transform.position;
+        if (rel.magnitude < 0.05f) return;
+        transform.Translate(Mathf.Sqrt(rel.magnitude * 2f) * rel.normalized * SpeedCoef * Time.deltaTime); // sqrt
+        //transform.Translate(rel * SpeedCoef * Time.deltaTime); // Linear
+        //transform.Translate(rel.magnitude * rel * SpeedCoef * Time.deltaTime); // square
+    }
+
+    private void FollowUpdate () {
+        if (player.FaceForward) {
+            MoveToUpdate(player.transform.position + PlayerRelCenterPos + Vector3.right * DirectionBias);
+        } else {
+
+            MoveToUpdate(player.transform.position + PlayerRelCenterPos + Vector3.left * DirectionBias);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        FollowUpdate();
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,25 +6,39 @@ public class PlayerController : MonoBehaviour {
 
     public static PlayerController Instance { get; private set; }
 
+    public float MoveSpeed = 2.5f;
+    public bool FaceForward { get; private set; }
+    public bool IsMoving { get; private set; }
+
     private CharacterController controller;
 
     private void Awake () {
         if (!Instance) Instance = this;
-        else Debug.LogWarning("Multiple player detected");
+        else Debug.LogWarning("Multiple PlayerController detected");
     }
 
     // Use this for initialization
     void Start () {
         controller = GetComponent<CharacterController>();
         if (!controller) Debug.LogWarning("No CharacterController");
+
+        IsMoving = false;
+        FaceForward = true;
     }
 
     private void InputUpdate () { // TEST
-        if (Input.GetKey(KeyCode.LeftArrow)) {
+        float movement = Input.GetAxis("Horizontal");
+        controller.SimpleMove(new Vector3(MoveSpeed * movement, 0f, 0f));
 
-        }
-        if (Input.GetKey(KeyCode.RightArrow)) {
-
+        if (movement == 0f) {
+            IsMoving = false;
+        } else {
+            IsMoving = true;
+            if (movement > 0f) {
+                FaceForward = true;
+            } else {
+                FaceForward = false;
+            }
         }
     }
 

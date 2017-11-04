88b0a12881cfa904647a47c3814024e3ebcf6a6c
Tianyi Zhao <tz1@andrew.cmu.edu>
2017-11-04 00:13:25 -0400

    abstract interactive trigger

---
+++ Assets/Scripts/InteractiveTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InteractiveTrigger : MonoBehaviour {
+
+    protected bool TriggerEnabled = true;
+    protected bool InteractiveEnabled = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    abstract protected void InteractionDown ();
+
+    abstract protected void PlayerTriggerEnter ();
+
+    abstract protected void PlayerTriggerExit ();
+
+    private void DetectInteraction () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (InteractiveEnabled) InteractionDown();
+        }
+    }
+
+    private void OnTriggerEnter (Collider other) {
+        if (other.tag == "Player") {
+            if (TriggerEnabled) PlayerTriggerEnter();
+        }
+    }
+
+    private void OnTriggerStay (Collider other) {
+        if (other.tag == "Player") {
+            if (TriggerEnabled) DetectInteraction();
+        }
+    }
+
+    private void OnTriggerExit (Collider other) {
+        if (other.tag == "Player") {
+            if (TriggerEnabled) PlayerTriggerExit();
+        }
+    }
+}
---
+++ Assets/Scripts/TestInteractiveTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInteractiveTrigger : InteractiveTrigger {
+
+    // Use this for initialization
+    void Start () {
+        InteractiveEnabled = true;
+        TriggerEnabled = true;
+    }
+
+    protected override void InteractionDown () {
+        Debug.Log("Fuck");
+    }
+
+    protected override void PlayerTriggerEnter () {
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    protected override void PlayerTriggerExit () {
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}

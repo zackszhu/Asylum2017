8165a70f7a46737721ab1656aac301aff5196620
Tianyi Zhao <tz1@andrew.cmu.edu>
2017-11-03 23:01:12 -0400

    trigger

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour { // TEST
+
+    private bool TriggerEnabled = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void DetectInteraction () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Fuck");
+        }
+    }
+
+    private void OnTriggerStay (Collider other) {
+        if (other.tag == "Player") {
+            TriggerEnabled = true;
+            transform.GetChild(0).gameObject.SetActive(true); // TEST
+            DetectInteraction();
+        }
+    }
+
+    private void OnTriggerExit (Collider other) {
+        if (other.tag == "Player") {
+            TriggerEnabled = false;
+            transform.GetChild(0).gameObject.SetActive(false); // TEST
+        }
+    }
+}

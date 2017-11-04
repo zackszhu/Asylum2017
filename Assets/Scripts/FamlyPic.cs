be3ab9b94dca9abbff6497bd9ebd3e6afe62ff20
Tianyi Zhao <tz1@andrew.cmu.edu>
2017-11-04 14:51:58 -0400

    family pic

---
+++ Assets/Scripts/FamlyPic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FamlyPic : MonoBehaviour {
+
+    [SerializeField] Material NormalMat;
+    [SerializeField] Material HorrorMat;
+
+    [SerializeField] Renderer PicRenderer;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void ShowHorror(bool show) {
+        if (show) {
+            PicRenderer.material = HorrorMat;
+        } else {
+            PicRenderer.material = NormalMat;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.H)) {
+            ShowHorror(true);
+        }
+	}
+}

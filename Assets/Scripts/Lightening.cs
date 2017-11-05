fbe30a1eaf16157b97d5dcd93c43e4f44cf61210
Tianyi Zhao <tz1@andrew.cmu.edu>
2017-11-05 18:03:36 -0500

    boy sounds

---
+++ Assets/Scripts/Lightening.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightening : MonoBehaviour {
+
+    private static Lightening Instance;
+    private Color CameraDefaultColor;
+    private Color LighteningColor;
+    private float LightIntensity;
+
+    [SerializeField] Camera MainCamera;
+    [SerializeField] Light LighteningLight;
+    [SerializeField] AudioSource LighteningAudio;
+
+    private void Awake () {
+        if (!Instance) Instance = this;
+        else Debug.LogWarning("Multiple Lightening Detected");
+    }
+
+    private void Start () {
+        CameraDefaultColor = MainCamera.backgroundColor;
+        LighteningColor = Color.white;
+        LightIntensity = LighteningLight.intensity;
+        LighteningLight.enabled = true;
+        LighteningLight.intensity = 0f;
+    }
+
+    public static void StartLightening () {
+        Instance.StartCoroutine(Instance.LighteningCoroutine());
+        Instance.LighteningAudio.Play();
+    }
+
+    IEnumerator LighteningCoroutine () {
+        yield return new WaitForSeconds(0.8f);
+        SetLightening(1f);
+        yield return new WaitForSeconds(0.1f);
+        SetLightening(0f);
+        yield return new WaitForSeconds(0.1f);
+        SetLightening(1f);
+        yield return new WaitForSeconds(0.1f);
+        SetLightening(0f);
+        yield return new WaitForSeconds(0.4f);
+        SetLightening(1f);
+        yield return new WaitForSeconds(0.3f);
+        SetLightening(0f);
+    }
+
+    private void SetLightening (float degree) {
+        MainCamera.backgroundColor = Color.Lerp(CameraDefaultColor, LighteningColor, degree);
+        LighteningLight.intensity = Mathf.Lerp(0f, LightIntensity, degree);
+    }
+
+    private void Update () {
+        // test 
+        if (Input.GetKeyDown(KeyCode.L)) {
+            StartLightening();
+        }
+    }
+}
---
+++ Assets/Scripts/SoundControllers/BoySoundController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoySoundController : MonoBehaviour {
+
+    private static BoySoundController Instance;
+
+    [SerializeField] AudioSource BoyAudio;
+    [SerializeField] AudioSource BreathAudio;
+    [SerializeField] AudioClip wakeup, breath_normal, breath_run;
+    [SerializeField] AudioClip[] emm;
+    [SerializeField] AudioClip[] mommy;
+    [SerializeField] AudioClip[] shocked;
+
+    private void Awake () {
+        if (!Instance) Instance = this;
+        else Debug.LogWarning("Multiple BoySound detected");
+    }
+
+    public static void PlayBreathNormal () {
+        Instance.BreathAudio.clip = Instance.breath_normal;
+        Instance.BreathAudio.Play();
+    }
+
+    public static void PlayBreathRun () {
+        Instance.BreathAudio.clip = Instance.breath_run;
+        Instance.BreathAudio.Play();
+    }
+
+    public static void StopBreath () {
+        Instance.BreathAudio.Stop();
+    }
+
+    public static void PlayWakeUp () {
+        Instance.BoyAudio.clip = Instance.wakeup;
+        Instance.BoyAudio.Play();
+    }
+
+    public static void PlayEmm (int index = -1) { // -1 is random
+        if (index >= 0 && index < Instance.emm.Length) {
+            Instance.BoyAudio.clip = Instance.emm[index];
+        } else {
+            Instance.BoyAudio.clip = Instance.emm[Random.Range(0, Instance.emm.Length)];
+        }
+        Instance.BoyAudio.Play();        
+    }
+
+    public static void PlayMommy (int index = -1) { // -1 is random
+        if (index >= 0 && index < Instance.mommy.Length) {
+            Instance.BoyAudio.clip = Instance.mommy[index];
+        } else {
+            Instance.BoyAudio.clip = Instance.mommy[Random.Range(0, Instance.mommy.Length)];
+        }
+        Instance.BoyAudio.Play();
+    }
+
+    public static void PlayShocked (int index = -1) { // -1 is random
+        if (index >= 0 && index < Instance.shocked.Length) {
+            Instance.BoyAudio.clip = Instance.shocked[index];
+        } else {
+            Instance.BoyAudio.clip = Instance.shocked[Random.Range(0, Instance.shocked.Length)];
+        }
+        Instance.BoyAudio.Play();
+    }
+}

4827125894ac384c9e26db268bb9d398b26def59
Tianyi Zhao <tz1@andrew.cmu.edu>
2017-11-04 21:06:26 -0400

    environment sound controller and more sound

---
+++ Assets/Scripts/SoundControllers/EnvironmentSoundController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentSoundController : MonoBehaviour {
+
+    public static EnvironmentSoundController Instance { get; private set; }
+
+    public AudioClip Indoor_wind, Outdoor_wind,
+        Indoor_rain, Outdoor_rain;
+
+    [SerializeField] AudioSource AmbientSource;
+
+    public static void SetAmbientSound (AudioClip clip) {
+        Instance.AmbientSource.clip = clip;
+        Instance.AmbientSource.Play();
+    }
+}
